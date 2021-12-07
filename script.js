6fd5af50ab305611026d996dd2c63ee772ac348f
jonnyhoudini <jmcaveety@gmail.com>
2021-12-07 06:54:26 +0000

    Added function to let user resize the grid.

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+
+// Define container
 const container = document.querySelector(".container");
 
-function createGrid() {
-  let i = 0;
-  while (i < 256) {
+function createGrid(size) {
+  for (i = 0; i < (size ** 2); i++) {
     const item = document.createElement("div");
     item.classList.add("item");
     item.addEventListener("mouseover", () => {
       item.style.backgroundColor = "white";
     });
     container.appendChild(item);
-    i++;
   }
+}
+
+createGrid(16);
+
+// Define clear button
+const btn = document.querySelector(".btncontainer");
+
+// Clear the grid
+
+btn.addEventListener("click", clearGrid);
+
+function clearGrid() {
+  let size = prompt("How big do you want your grid to be (1-100)");
+  if (size > 100 || size < 1) {
+    size = prompt("How big do you want your grid to be (1-100)");
+  };
+  container.innerHTML = "";
+  container.style.gridTemplateColumns = `repeat(${size}, 10px)`;
+  container.style.gridTemplateRows = `repeat(${size}, 10px)`;
+  createGrid(size);
 
 }
 
-createGrid();
+
+
+// Ask for new size and save it as a variable
